c591637b9c97e8274e230631cc93d2f0a23367d3
Timur Sadullayev <nstwf.pro@gmail.com>
2022-12-22 14:45:20 +0700

    Chore: typo fix in PoolInterface

--- src/PoolInterface.php
+++ src/PoolInterface.php
@@ -16,7 +16,7 @@ interface PoolInterface
     /**
      * Return connection and occupy it
      *
-     * Do not forget to release connection to the pool
+     * **Do not forget to release connection to the pool**
      *
      * ```php
      * $pool->getConnection()
@@ -24,7 +24,7 @@ interface PoolInterface
      *          $connection->query("UPDATE users SET blocked = 1 WHERE id = 3");
      *
      *          $pool->releaseConnection($connection);
-     *      })
+     *      });
      * ```
      *
      * @return PromiseInterface<ConnectionInterface>
